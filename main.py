e677ed8f9304a3bfada730679d99cfdac5f07281
huytrinh03 <trinhnhathuy03@gmail.com>
2023-01-16 00:30:27 -0500

    edit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from astropy.timeseries import LombScargle
-from astropy import units
-from astropy.io import fits
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
 
 def csv_conversion(file_directory, filename):
     fields = ['apogee_id', 'period (days)']
@@ -29,8 +26,6 @@ def computePeriod(csv_file):
     for row in csvreader:
         data.append(row)
 
-    # apogee_id  apstar_id ra  dec nvisits starSNR vscatter  verr  visit_id  vhelio  vrelerr visitSNR  starflag  mjd
-    # 2M00000662+7528598 apogee.apo25m.stars.120+12.2M00000662+7528598 0.027622  75.483292 9 827.6752  2.006593  0.08394658  apogee.apo25m.dr17.7545.56933.190 15.74034  0.07290228  233.139 1179648 56933
     period = np.empty([0,2])  # An array to store the period corresponding to each star (apogee_id)
     i=0
     while (i < len(data)):
@@ -42,18 +37,13 @@ def computePeriod(csv_file):
         while (i <= len(data)-1 and data[i][0] == apogee_id):  # iterate through each star using the star's apogee_id
             time = np.append(time, int(data[i][13]))  # Append each visit's data
             vhelio = np.append(vhelio, float(data[i][9]))
-            #plt.plot(time, vhelio, 'o')
-            #plt.show()
             vrelerr = np.append(vrelerr, float(data[i][10]))
             i += 1
 
         if (len(time) <= 1):
             continue
 
-        #frequency, power = LombScargle(time, vhelio, vrelerr).autopower(minimum_frequency=0.01, maximum_frequency=24,samples_per_peak=10)  # Compute Lomb-Scargle power
         frequency, power = LombScargle(time, vhelio, vrelerr).autopower(maximum_frequency=1 )
-        #plt.plot(frequency, power)
-        #plt.show()
         max_frequency = frequency[np.argmax(power)]  # Find the frequency with max power
         max_period = np.round(1.0/max_frequency, 2)
         period = np.append(period, [[apogee_id, max_period]], axis=0)  # Append to the period array
@@ -61,5 +51,4 @@ def computePeriod(csv_file):
     return period
 
 if __name__ == "__main__":
-    #print(computePeriod('/Users/trinhnhathuy/Documents/2.McGill/Academic/3.2022Fall/SideProject_SDSS/binaryStarVisit100000.csv'))
-    csv_conversion('/Users/trinhnhathuy/Documents/binaryStarVisit100000.csv', 'binaryStarPeriod100000.csv')
+    csv_conversion('/Users/trinhnhathuy/Documents/2.McGill/Academic/3.2022Fall/SideProject_SDSS/binaryStarVisit100000.csv', 'binaryStarPeriod100000.csv')
